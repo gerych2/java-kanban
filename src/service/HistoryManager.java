4aea54716bc163750baa8e64818a539c7914f0ae
German-Trosko <skodagrodno2006@gmail.com>
2025-05-23 22:32:03 +0300

    конечное 38

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -4,7 +4,11 @@ import model.Task;
 import java.util.Collection;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     Collection<Task> getHistory();
+
     void remove(int id);
+
 }
