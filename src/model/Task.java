851673c1454a3b588d03e4f2dbfbd8e1985b06c2
German-Trosko <skodagrodno2006@gmail.com>
2025-06-02 12:52:53 +0300

    7 спринт попытка 6

--- src/model/Task.java
+++ src/model/Task.java
@@ -75,10 +75,11 @@ public abstract class Task implements Cloneable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Task)) return false;
         Task task = (Task) o;
         return Objects.equals(id, task.id);
     }
+
 
     @Override
     public int hashCode() {
