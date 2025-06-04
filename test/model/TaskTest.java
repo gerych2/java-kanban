425562646fc45a27162e719afe1b93f4126ba807
German-Trosko <skodagrodno2006@gmail.com>
2025-06-05 02:22:10 +0300

    7 спринт попытка 10

--- test/model/TaskTest.java
+++ test/model/TaskTest.java
@@ -7,30 +7,13 @@ class TaskTest {
 
     @Test
     void tasksWithSameIdShouldBeEqual() {
-        // Создаем первую задачу через анонимный класс
-        Task task1 = new Task("Test", "Description", TaskStatus.NEW, TaskType.TASK) {
-            @Override
-            public String toCsvString() {
-                return String.format("%d,%s,%s,%s,%s,",
-                        getId(), getType(), getName(), getTaskStatus(), getDescription());
-            }
-        };
+        Task task1 = new SimpleTask("Test", "Description", TaskStatus.NEW);
         task1.setId(1);
 
-        // Создаем вторую задачу с тем же id, но разными другими полями
-        Task task2 = new Task("Another Test", "Another Description", TaskStatus.IN_PROGRESS, TaskType.TASK) {
-            @Override
-            public String toCsvString() {
-                return String.format("%d,%s,%s,%s,%s,",
-                        getId(), getType(), getName(), getTaskStatus(), getDescription());
-            }
-        };
+        Task task2 = new SimpleTask("Another Test", "Another Description", TaskStatus.IN_PROGRESS);
         task2.setId(1);
 
-        // Проверяем равенство через equals()
         assertTrue(task1.equals(task2), "Задачи с одинаковым id должны быть равны");
-
-        // Дополнительно проверяем через assertEquals (чтобы убедиться, что тест проходит)
         assertEquals(task1, task2, "Задачи с одинаковым id должны быть равны");
     }
 }
