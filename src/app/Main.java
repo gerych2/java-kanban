fb8035fddd542640995686079348f6d2651de751
German-Trosko <skodagrodno2006@gmail.com>
2025-06-05 23:25:12 +0300

    7 спринт попытка 12

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,14 +5,24 @@ import service.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем SimpleTask вместо анонимного класса
-        SimpleTask task1 = new SimpleTask("Переезд", "Упаковать вещи", TaskStatus.NEW);
+        Task task1 = new Task("Переезд", "Упаковать вещи", TaskStatus.NEW) {
+            @Override
+            public TaskType getType() {
+                return TaskType.TASK;
+            }
+        };
         task1.setId(1);
 
-        SimpleTask task2 = new SimpleTask("Покупка продуктов", "Купить хлеб и молоко", TaskStatus.NEW);
+        Task task2 = new Task("Покупка продуктов", "Купить хлеб и молоко", TaskStatus.NEW) {
+            @Override
+            public TaskType getType() {
+                return TaskType.TASK;
+            }
+        };
         task2.setId(2);
 
-        System.out.println(task1.toCsvString());
-        System.out.println(task2.toCsvString());
+        // Используем CsvConverter
+        System.out.println(CsvConverter.toCsvString(task1));
+        System.out.println(CsvConverter.toCsvString(task2));
     }
 }
